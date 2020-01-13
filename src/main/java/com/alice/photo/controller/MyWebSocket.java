8fbc44d6171c699c2ba575895434d544ed7a8bc1
高海军 <gaohaijun@ebigdata.org>
2020-01-13 09:27:02 +0800

    <feat>websocket修改

--- src/main/java/com/alice/photo/controller/MyWebSocket.java
+++ src/main/java/com/alice/photo/controller/MyWebSocket.java
@@ -1,94 +1,84 @@
-//package com.alice.photo.controller;
-//
-//import javax.websocket.*;
-//import javax.websocket.server.ServerEndpoint;
-//import java.io.IOException;
-//import java.util.Map;
-//import java.util.concurrent.ConcurrentHashMap;
-//import java.util.concurrent.CopyOnWriteArraySet;
-//
-///**
-// * @author ghj
-// * @Description websocket服务器端
-// * @date 2020/1/10 10:52
-// */
-//@ServerEndpoint("/websocket")
-//public class MyWebSocket {
-//    private int onlineCount = 0;
-//    //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
-//    // 若要实现服务端与单一客户端通信的话，可以使用Map来存放，其中Key可以为用户标
-////    private static CopyOnWriteArraySet<MyWebSocket> webSocketSet = new CopyOnWriteArraySet<MyWebSocket>();
-//    private static Map<Long, MyWebSocket> webSocketMap = new ConcurrentHashMap<Long, MyWebSocket>();
-//    private Session session;
-//
-//    @OnOpen
-//    public void onOpen(Session session) {
-//        this.session = session;
-//        webSocketMap.put(System.currentTimeMillis(), this);     //加入set中
-//        addOnlineCount();           //在线数加1
-//        System.out.println("有新连接加入！当前在线人数为" + webSocketMap.size());
-//    }
-//
-//    @OnClose
-//    public void onClose() {
-//        webSocketMap.remove(session.getId());
-//        subOnlineCount();
-//        System.out.println("有一连接关闭！当前在线人数为" + webSocketMap.size());
-//    }
-//
-//    /**
-//     * 发送信息
-//     *
-//     * @param content
-//     * @param session
-//     */
-//    @OnMessage
-//    public void onMessage(String content, Session session) {
-//        System.out.println("来自客户端的消息:" + content);
-//        webSocketMap.forEach((key, myWebSocket) -> {
-//            try {
-//                myWebSocket.sendMsg(content, key);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//
-//            }
-//        });
-////        for (MyWebSocket webSocket : webSocketMap) {
-////            try {
-////                webSocket.sendMsg(content, webSocket);
-////            } catch (Exception e) {
-////                e.printStackTrace();
-////                continue;
-////            }
-////        }
-//    }
-//
-//    /**
-//     * 发生错误时调用
-//     *
-//     * @param session
-//     * @param error
-//     */
-//    @OnError
-//    public void onError(Session session, Throwable error) throws IOException {
-//        System.out.println("发生错误");
-//        session.close();
-//        error.printStackTrace();
-//    }
-//
-//    private void sendMsg(String content, Long info) throws IOException {
-//        this.session.getBasicRemote().sendText(content);
-//    }
-//
-//    private void subOnlineCount() {
-//        onlineCount--;
-//    }
-//
-//    private int getOnlineCount() {
-//        return this.onlineCount;
-//    }
-//
-//    private void addOnlineCount() {
-//        onlineCount++;
-//    }
-//}
+package com.alice.photo.controller;
+
+import com.alice.photo.model.dto.Message;
+import com.google.gson.Gson;
+
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author ghj
+ * @Description
+ * @date 2020/1/10 15:48
+ */
+@ServerEndpoint("/websocket")
+public class MyWebSocket {
+    public static Map<String, Session> sessionMap = new HashMap<String, Session>();
+    private Session session;
+
+    @OnOpen
+    public void startSocket(Session session) {
+        this.session = session;
+        System.out.println("链接成功");
+        if (sessionMap.size() == 0) {
+            return ;
+        }
+        Set userIds = sessionMap.keySet();
+        StringBuffer sBuffer  = new StringBuffer();
+        for (Object str : userIds) {
+            sBuffer.append(str.toString() + ":");
+        }
+        Gson gson = new Gson();
+        try {
+            Message message = new Message();
+            message.setFrom("系统");
+            message.setMsg(sBuffer.toString());
+            session.getBasicRemote().sendText(gson.toJson(message),true);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @OnMessage
+    public void getMessgae(String str,Session session) {
+        if (session.isOpen()) {
+            try {
+                System.out.println(str);
+                Gson gson = new Gson();
+                Message msg = gson.fromJson(str, Message.class);
+                Message toMessage = msg;
+                toMessage.setFrom(msg.getId());
+                toMessage.setTo(msg.getTo());
+
+                if (msg.getMsg().equals("newUser")) {
+                    if (sessionMap.containsKey(msg.getId())) {
+                        sessionMap.remove(msg.getId());
+                    }
+                    sessionMap.put(msg.getId(), session);
+                } else {
+                    Session toSession = sessionMap.get(msg.getTo());
+                    if (toSession != null && toSession.isOpen()) {
+                        toSession.getBasicRemote().sendText(gson.toJson(toMessage));
+                    } else {
+                        toMessage.setMsg("用户不存在");
+                        toMessage.setFrom("系统");
+                        session.getBasicRemote().sendText(gson.toJson(toMessage));
+                    }
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        } else {
+            System.out.println("session is closed");
+        }
+    }
+}
